42b1f7e4aece3b7dde7a70dfa0f8087e03c94fbf
OodavidsinoO <david.sin@tuta.io>
2025-01-06 15:31:13 +0800

    Add custom remote server and llm model support

--- chat_with_netty.py
+++ chat_with_netty.py
@@ -27,7 +27,9 @@ import shelve
 
 # ======== Search Engine Functions ========
 from langchain_community.tools import DuckDuckGoSearchResults
-from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
+
+# ======== Prompt Texts ========
+import nettyPrompts
 
 ################################################################################
 # Constant values for the RAG model.
@@ -40,57 +42,28 @@ logger.info(f"Loaded .env file successfully.")
 # 8 is usually a good number.
 REFERENCE_COUNT = 8
 
-# If the user did not provide a query, we will use this default query.
-_default_query = "What is the ultimate answer to life, the universe, and everything?"
-
-# This is really the most important part of the rag model. It gives instructions
-# to the model on how to generate the answer. Of course, different models may
-# behave differently, and we haven't tuned the prompt to make it optimal - this
-# is left to you, application creators, as an open problem.
-_rag_query_text = """
-You are a large language AI assistant built by HKUST Students for Final Year Project. You are given a user question, and please write clean, concise and accurate answer to the question. You will be given a set of related contexts to the question, each starting with a reference number like [[citation:x]], where x is a number. Please use the context and cite the context at the end of each sentence if applicable.
-
-Your answer must be correct, accurate and written by an expert using an unbiased and professional tone. Please limit to 1024 tokens. Do not give any information that is not related to the question, and do not repeat. Say "information is missing on" followed by the related topic, if the given context do not provide sufficient information.
-
-Please cite the contexts with the reference numbers, in the format [citation:x]. If a sentence comes from multiple contexts, please list all applicable citations, like [citation:3][citation:5]. Other than code and specific names and citations, your answer must be written in the same language as the question.
-
-Here are the set of contexts:
-
-{context}
-
-Remember, don't blindly repeat the contexts verbatim. And here is the user question:
-"""
-
 # A set of stop words to use - this is not a complete set, and you may want to
 # add more given your observation.
-stop_words = [
-    "<|im_end|>",
-    "[End]",
-    "[end]",
-    "\nReferences:\n",
-    "\nSources:\n",
-    "End.",
-]
-
-# This is the prompt that asks the model to generate related questions to the
-# original question and the contexts.
-# Ideally, one want to include both the original question and the answer from the
-# model, but we are not doing that here: if we need to wait for the answer, then
-# the generation of the related questions will usually have to start only after
-# the whole answer is generated. This creates a noticeable delay in the response
-# time. As a result, and as you will see in the code, we will be sending out two
-# consecutive requests to the model: one for the answer, and one for the related
-# questions. This is not ideal, but it is a good tradeoff between response time
-# and quality.
-_more_questions_prompt = """
-You are a helpful assistant that helps the user to ask related questions, based on user's original question and the related contexts. Please identify worthwhile topics that can be follow-ups, and write questions no longer than 20 words each. Please make sure that specifics, like events, names, locations, are included in follow up questions so they can be asked standalone. For example, if the original question asks about "the Manhattan project", in the follow up question, do not just say "the project", but use the full name "the Manhattan project". Your related questions must be in the same language as the original question.
-
-Here are the contexts of the question:
-
-{context}
-
-Remember, based on the original question and related contexts, suggest three such further questions. Do NOT repeat the original question. Each related question should be no longer than 20 words. Here is the original question:
-"""
+if os.environ.get("LLM_USE_CUSTOM_SERVER"):
+    # For custom models, we will use a different set of stop words.
+    stop_words = [
+        # "<|im_end|>",
+        "[End]",
+        # "[end]",
+        "\nReferences:\n",
+        "\nSources:\n",
+        "End.",
+    ]
+else:
+    # OpenAI API only supports the four stop words below.
+    stop_words = [
+        "<|im_end|>",
+        "[End]",
+        "[end]",
+        "\nReferences:\n",
+        "\nSources:\n",
+        "End.",
+    ]
 
 # ======== Search Engine (ARAG) Functions ========
 
@@ -176,15 +149,25 @@ class RAG(Photon):
         try:
             return thread_local.client
         except AttributeError:
-            thread_local.client = openai.OpenAI(
-                base_url=f"https://{self.model}.lepton.run/api/v1/",
-                api_key=os.environ.get("LEPTON_WORKSPACE_TOKEN")
-                or WorkspaceInfoLocalRecord.get_current_workspace_token(),
-                # We will set the connect timeout to be 10 seconds, and read/write
-                # timeout to be 120 seconds, in case the inference server is
-                # overloaded.
-                timeout=httpx.Timeout(connect=10, read=120, write=120, pool=10),
-            )
+            if not os.environ.get("LLM_USE_CUSTOM_SERVER"):
+                logger.info("Using Lepton LLM model.")
+                thread_local.client = openai.OpenAI(
+                    base_url=f"https://{self.model}.lepton.run/api/v1/",
+                    api_key=os.environ.get("LEPTON_WORKSPACE_TOKEN")
+                    or WorkspaceInfoLocalRecord.get_current_workspace_token(),
+                    # We will set the connect timeout to be 10 seconds, and read/write
+                    # timeout to be 120 seconds, in case the inference server is
+                    # overloaded.
+                    timeout=httpx.Timeout(connect=10, read=120, write=120, pool=10),
+                )
+            else:
+                logger.info(f"Using custom LLM model. Remote URL: {os.environ['LLM_REMOTE_URL']}")
+                thread_local.client = openai.OpenAI(
+                    base_url=os.environ["LLM_REMOTE_URL"],
+                    api_key=os.environ.get("LLM_REMOTE_API_KEY"),
+                    timeout=httpx.Timeout(connect=10, read=120, write=120, pool=10),
+                )
+
             return thread_local.client
 
     def init(self):
@@ -210,7 +193,11 @@ class RAG(Photon):
             )
         else:
             raise RuntimeError(f"Unknown backend. Please check the environment variable. self.backend: {self.backend}")
-        self.model = os.environ["LLM_MODEL"]
+        # Set the LLM model to use.
+        if os.environ.get("LLM_USE_CUSTOM_SERVER"):
+            self.model = os.environ["LLM_REMOTE_MODEL"]
+        else:
+            self.model = os.environ["LLM_MODEL"]
         # An executor to carry out async tasks, such as uploading to KV.
         self.executor = concurrent.futures.ThreadPoolExecutor(
             max_workers=self.handler_max_concurrency * 2
@@ -250,7 +237,7 @@ class RAG(Photon):
                 messages=[
                     {
                         "role": "system",
-                        "content": _more_questions_prompt.format(
+                        "content": nettyPrompts._more_questions_prompt.format(
                             context="\n\n".join([c["snippet"] for c in contexts])
                         ),
                     },
@@ -269,11 +256,15 @@ class RAG(Photon):
             if isinstance(related, str):
                 related = json.loads(related)
             logger.trace(f"Related questions: {related}")
-            return related["questions"][:5]
+            return related["questions"]
+        except TypeError as e:
+            # No related questions found.
+            logger.info(f"No related questions found: {e}")
+            return []
         except Exception as e:
             # For any exceptions, we will just return an empty list.
             logger.error(
-                "encountered error while generating related questions:"
+                "Encountered error while generating related questions:"
                 f" {e}\n{traceback.format_exc()}"
             )
             return []
@@ -380,14 +371,14 @@ class RAG(Photon):
             return StreamingResponse(content=result, media_type="text/html")
 
         # First, do a search query.
-        query = query or _default_query
+        query = query or nettyPrompts._default_query
         # Basic attack protection: remove "[INST]" or "[/INST]" from the query
         query = re.sub(r"\[/?INST\]", "", query)
         contexts = self.search_function(query)
         # DEBUG: print the contexts.
-        logger.debug(f"Contexts: {contexts}")
+        logger.debug(f"Contexts: \n{json.dumps(contexts, sort_keys = True, indent = 4)}")
 
-        system_prompt = _rag_query_text.format(
+        system_prompt = nettyPrompts._rag_query_text.format(
             context="\n\n".join(
                 [f"[[citation:{i+1}]] {c['snippet']}" for i, c in enumerate(contexts)]
             )
@@ -438,4 +429,4 @@ class RAG(Photon):
 
 if __name__ == "__main__":
     rag = RAG()
-    rag.launch()
+    rag.launch(host = "127.0.0.1", port = 80)
